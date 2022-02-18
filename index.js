71ca1e149d23d9d176c4ad1361f8922b7fa22d29
shayan <sak704@nyu.edu>
2022-02-18 09:30:33 -0800

    commit tree 5201bd11c7b14b209644fed88d13a2232609fd22

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 // code your solution here
 
-const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    //...
-  ]
-let winYear;
-let jost;
-function winTest(element,index,array) {
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+//let winYear;
+
+function winTest(element) {
     return element["result"] === "W"
 }
 function superbowlWin(array) {
     //console.log(element)
     
-    
-    let plcHolder = array;   
+    // let jost;
+    // let plcHolder = array;   
         
     // } else{
     //    undefined);
     // }
 
    //console.log(record)
-    jost = plcHolder.find(winTest);
+    let jost = array.find(winTest);
     
+    return jost && jost["year"]
+
+    return !!jost ? jost["year"] : jost
+
+
     if (jost === undefined) {
         return undefined;
     } else {
