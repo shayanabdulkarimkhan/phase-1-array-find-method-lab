867a4a64cc3aea82f1e99fc60bb14339b690c002
shayan <sak704@nyu.edu>
2022-02-17 19:12:43 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+let winYear;
+let jost;
+function winTest(element,index,array) {
+    return element["result"] === "W"
+}
+function superbowlWin(array) {
+    //console.log(element)
+    
+    
+    let plcHolder = array;   
+        
+    // } else{
+    //    undefined);
+    // }
+
+   //console.log(record)
+    jost = plcHolder.find(winTest);
+    
+    if (jost === undefined) {
+        return undefined;
+    } else {
+        return jost["year"];
+    }   
+       
+}
+
+
+
+
+
+
+    
